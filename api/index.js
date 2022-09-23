9b5c1a955d55bb614a4fc9a99c469f955d7300ea
to-ge <tougei324@gmail.com>
2022-09-24 00:56:17 +0900

    confirm

--- api/index.js
+++ api/index.js
@@ -9,7 +9,10 @@ const cors = require("cors");
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: "true",
+    useUnifiedTopology: "true",
+  })
   .then(() => console.log("DB connection successfull."))
   .catch((err) => console.log(err));
 
--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import Home from "./pages/home/Home";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Music from "./pages/music/Music";
 import MLB from "./pages/MLB/MLB";
 import PriorityList from "./components/priorityList/PriorityList";
