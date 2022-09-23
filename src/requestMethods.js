e965f56496d49d84a8d2bd463c7652b6c079819a
to-ge <tougei324@gmail.com>
2022-09-24 00:20:51 +0900

    add command

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const TODO_URL = "https://togeportfolio.herokuapp.com/api/todos";
-const MLB_URL = "https://togeportfolio.herokuapp.com/api/mlb";
+const TODO_URL = "/api/todos";
+const MLB_URL = "/api/mlb";
 
 export const todoRequest = axios.create({
   baseURL: TODO_URL,
